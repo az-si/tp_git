3ee140d19eb44ff27f11d6aef085bf43860fec21
az-si <az.si.box@gmail.com>
2025-03-14 10:37:00 +0100

    tp's first commit

---
+++ readme.mk
@@ -0,0 +1,11 @@
+Tp Git with le Thomas 🖥️
+---------------------------------------------------------
+following to the creation of a repo in GitHub :
+
+1. creating a folder in my cp,
+	=> it will create a classic folder
+2. creating a readme markdown file 
+3. intializing the repo GitHub in local dir
+
+```git init```
+that cmd will intialize the repo git in the local dir
